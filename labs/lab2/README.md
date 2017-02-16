b0284ec17101eee74d3d9e3d597ce2b898b2d3d6
dawneraq <dawneraq@gmail.com>
2017-02-16 01:14:56 -0500

    Finish lab2

--- labs/lab2/README.md
+++ labs/lab2/README.md
@@ -1,7 +1,10 @@
+# References
+http://stackoverflow.com/questions/9989382/add-1-to-current-date
+
 # Questions
 
 # Observations
-This lab only works with Firefox, due to the following errors in Chrome and Safari, respectively:
+~~~This lab only works with Firefox, due to the following errors in Chrome and Safari, respectively:~~~
 	XMLHttpRequest cannot load https://api.darksky.net/forecast/1be6013062da72c0042892180c2f1f09/42.7495,-73.5951. Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://websci' is therefore not allowed access.
 	
 	[Error] Failed to load resource: Origin http://websci is not allowed by Access-Control-Allow-Origin. (42.7495,-73.5951, line 0)
@@ -12,3 +15,5 @@ CORS errors are the bane of my web developer existence.
 Making the DarkSky API request with HTTP instead of HTTPS just returned an empty response, so I switched to the Open Weather API after two hours of trying.
 
 In the lab directions, "scroll the forecast in a separate area of the page" was unclear at first.
+
+Sometimes the forecast result from $.getJSON is undefined. Maybe this has something to do with the maximum number of Open Weather API calls I can make per second.
--- labs/lab2/weather.js
+++ labs/lab2/weather.js
@@ -3,46 +3,63 @@ var apiKey = '43d590218aa74d6e974ced753c0419ec';
 var currentLocation = {};
 var weather = {};
 var temperature;
+var forecastCount = 10;
+var monthNames = [
+	'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+];
+var today = new Date();
 
-$(document).ready(function() {	
-	setLocation();
+$(document).ready(function() {
+	$('#todays-date').text(simpleDate(today));
+	initWeatherApp();
 });
 
-function setLocation() {
-	// Get the current location
-	$.getJSON('http://ip-api.com/json', function(data) {
-		currentLocation.latitude = data.lat;
-		currentLocation.longitude = data.lon;
-		$('#latitude').val(currentLocation.latitude);
-		$('#longitude').val(currentLocation.longitude);
-	}).then(function() {
-		// Get weather data from OpenWeatherMap
-		let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?&APPID=' + apiKey + '&lat=' + currentLocation.latitude + '&lon=' + currentLocation.longitude;
-		$.getJSON(apiUrl, function(data) {
-			console.log(JSON.stringify(data, null, 4));
+function initWeatherApp() {
+	// Set the current location
+	$.getJSON('http://ip-api.com/json',
+		setLocation
+	).then(function() {
+		// Get weather data from OpenWeatherMap for current weather
+		let currentWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather?&APPID=' + apiKey + '&lat=' + currentLocation.latitude + '&lon=' + currentLocation.longitude;
+		$.getJSON(currentWeatherUrl, function(data) {
+			//console.log(JSON.stringify(data, null, 4));
 			weather = data;
 		}).then(function() {
 			// Populate weather data in HTML
 			deepWeather = weather.weather[0];
-			temperature = decimal2(weather.main.temp);
+			temperature = weather.main.temp;
 			
 			$('#weather-main').text(deepWeather.main);
 			$('#weather-description').text(deepWeather.description);
-			$('#weather-icon').attr('src', 'http://openweathermap.org/img/w/' + deepWeather.icon + '.png');
+			$('#weather-icon').attr('src', openWeatherIconUrl(deepWeather.icon));
 			$('#city').val(weather.name);
 			
-			$('#temperature').text(temperature + ' ' + degreeSymbol + 'F');
+			$('#temperature').text(tempString(temperature));
 			$('#humidity').text(weather.main.humidity + '%');
 			
 			$('#wind').text(decimal2(msToMph(weather.wind.speed)) + ' mph @ ' + weather.wind.deg + ' ' + degreeSymbol);
-			
-			// Color the background according to the current temperature
-			var hexRed = (256 - (136 - Math.round(temperature))) % 256;
-			var hexBlue = (256 - (-126 - Math.round(temperature))) % 256;
-			var tempColor = 'rgb(' + hexRed + ', 0, ' + hexBlue + ')';
-			$('body').css('background', 'linear-gradient(white, ' + tempColor + ')');
+		}).then(
+			setBgColor
+		).then(function() {
+			let forecastUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?APPID=' + apiKey + '&lat=' + currentLocation.latitude + '&lon=' + currentLocation.longitude + '&cnt=' + forecastCount;
+			$.getJSON(forecastUrl, function(data) {
+				$('#forecast').empty();
+				let forecast = data.list;
+				//console.log(forecast);
+				forecast.forEach(function(forecastWeather, forecastIndex) {
+					$('#forecast').append(forecastCardTemplate(forecastWeather, forecastIndex));
+				});
+			});
 		});
 	});
+}
+
+function setLocation(data) {
+	currentLocation.latitude = data.lat;
+	currentLocation.longitude = data.lon;
+	$('#latitude').val(currentLocation.latitude);
+	$('#longitude').val(currentLocation.longitude);
 }
 
 function kToF(k) {
@@ -55,4 +72,39 @@ function msToMph(s) {
 
 function decimal2(n) {
 	return Math.round(n * 100) / 100;
+}
+
+function tempString(k) {
+	return decimal2(kToF(k)) + ' ' + degreeSymbol + 'F';
+}
+
+function setBgColor() {
+	// Color the background according to the current temperature
+	var hexRed = (256 - (136 - Math.round(temperature))) % 256;
+	var hexBlue = (256 - (-126 - Math.round(temperature))) % 256;
+	var tempColor = 'rgb(' + hexRed + ', 0, ' + hexBlue + ')';
+	$('body').css('background', 'linear-gradient(white, ' + tempColor + ')');
+}
+
+function openWeatherIconUrl(iconCode) {
+	return 'http://openweathermap.org/img/w/' + iconCode + '.png';
+}
+
+function forecastCardTemplate(data, index) {
+	//console.log(data);
+	let forecastDeepWeather = data.weather[0];
+	//console.log(forecastDeepWeather);
+	let newDate = new Date(today.getTime());
+	newDate.setDate(newDate.getDate() + 1 + index);
+	return `<div class="forecast-card" class="col-md-2">
+			<h4>${simpleDate(newDate)}</h4>
+			<img src='${openWeatherIconUrl(forecastDeepWeather.icon)}'>
+			<span>${tempString(data.temp.min)} - ${tempString(data.temp.max)}</span><br/>
+			<span>${forecastDeepWeather.main}</span>
+			<span>${forecastDeepWeather.description}</span>
+		</div>`;
+}
+
+function simpleDate(d) {
+	return monthNames[d.getMonth()] + ' ' + d.getDate();
 }
