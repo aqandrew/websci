f68b45ed8e0353818c8cda9f504bd3d450f39359
dawneraq <dawneraq@gmail.com>
2017-03-23 16:33:29 -0400

    Handle getTweets POST clientside

--- labs/lab5/lab5.js
+++ labs/lab5/lab5.js
@@ -5,6 +5,19 @@ function Lab5Controller($scope, $http) {
   $scope.appName = 'Lab 6';
   $scope.query = '';
   $scope.tweetNum = 5;
+
+  $('form').submit(event => {
+    event.preventDefault(); // Prevent page redirect on submit
+
+    let postData = {
+      query: $scope.query,
+      tweetNum: $scope.tweetNum
+    };
+
+    $.post('/getTweets', postData, response => {
+      console.log(response);
+    });
+  });
 }
 
 Lab5Controller.$inject = ['$scope', '$http'];
