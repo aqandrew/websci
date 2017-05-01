a741b18c7097193274d4cc8ca8371825f37ac61c
dawneraq <dawneraq@gmail.com>
2017-05-01 16:30:40 -0400

    Create quiz2

---
+++ quizzes/aquina-quiz2/README.md
@@ -0,0 +1,3 @@
+# Quiz 2
+
+Before running `npm start`, make sure you have `mongod` running in another command prompt.
---
+++ quizzes/aquina-quiz2/server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Load dependencies
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const request = require('request');
+const mongoose = require('mongoose');
+const dbName = 'quiz2';
+const db = mongoose.connect('mongodb://localhost/' + dbName);
+
+const degreeSymbol = '\xB0';
+const apiKey = '43d590218aa74d6e974ced753c0419ec';
+const port = 8000;
+
+// Store minimum required fields for Weather
+var weatherSchema = new mongoose.Schema({
+  zipcode: 'number',
+  locationName: 'string',
+  temperature: 'number'
+});
+var Weather = mongoose.model('Weather', weatherSchema);
+
+// Set root folder
+app.use(express.static(__dirname));
+
+// Parse JSON-encoded HTTP requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+
+
+server.listen(port, () => {
+  Weather.remove().exec(); // Start with no weather info
+	console.log('quiz2 server listening on port ' + port);
+});
